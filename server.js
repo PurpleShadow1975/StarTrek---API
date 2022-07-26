e2350c92caf190bdc4a760776c2654784549e787
Antje Myriknas <antje.myriknas@yahoo.co.uk>
2022-07-26 12:57:30 +0100

    setup

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const PORT = 3000;
+
+app.use(cors());
+
+const aliens = {
+  humans: {},
+  vulcans: {},
+  unknown: {},
+  borg: {},
+  klingons: {},
+  romulans: {},
+  gorn: {},
+  trill: {},
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:alienName", (req, res) => {
+  const aliensName = req.params.alienName.toLowerCase();
+  if (aliens[aliensName]) {
+    res.json(aliens[aliensName]);
+  } else {
+    res.json(aliens["unknown"]);
+  }
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server is running!`);
+});
